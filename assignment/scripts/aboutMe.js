1531e3250b6830388af7f4ecb19bb09ecf637fa3
Miski Ali <miskiali222@gmail.com>
2022-05-11 20:49:37 -0500

    I have done questions 1 through 12.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,25 +7,28 @@ let lastNameVar = 'Ali';
 // (remember, you already have variables for this, can you use those?)
 let fullNameVar= 'Miski'+'Ali';
 // 4 - Console log the value of `fullName`
-Console.log(let fullNameVar = 'Miski'+'Ali');
+console.log ('the value of fullNameVar is', fullNameVar);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
+let luckyNumberVar = 22;
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
+console.log ('My name is', fullNameVar); 
+console.log (', and I think', luckyNumberVar); 
+console.log ('is a winner!');
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurousVar = true; 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let foodVar = 'Street Tacos';
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let petsVar= 0; 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPetsVar = 1;
 // 11 - Add two pets to your `pets` variable
-
+petsVar+=2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets= 4;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
